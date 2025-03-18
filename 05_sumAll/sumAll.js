4c7213072fe8610b90d8c4ce65b0709684b0747b
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-03-18 22:23:24 +0100

    Acomplish with even better functioning than expect

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+
+    //The below should be uncommented to the function trasform "number" to Number
+    // num1 = Number(num1);
+    // num2 = Number(num2);
+
+    if (isNaN(num1) || isNaN(num1)){
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    else{
+        if(num1 === num2) {
+        sum = num1;
+        return sum;
+    }
+    else if (num1 > num2){
+        for(i = num2; i <= num1; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else if (num2 > num1){
+        for(i = num1; i <= num2; i++){
+            sum += i;
+        }
+        return sum;
+    }
+}
 
 };
 
