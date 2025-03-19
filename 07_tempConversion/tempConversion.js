635b89a0701190120f17578b842fc9d5de984e57
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-03-19 20:13:32 +0100

    Pass temp convertion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+  let celcTemp = (parseFloat(fahrTemp.toFixed(2)) - 32) * 5/9;
+  let fixedCelcTemp = parseFloat(celcTemp.toFixed(1));
+  return fixedCelcTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcTemp) {
+  let fahrTemp = (parseFloat(celcTemp.toFixed(2)) * 9/5) + 32;
+  let fixedfahrTemp = parseFloat(fahrTemp.toFixed(1));
+  return fixedfahrTemp;
 };
 
 // Do not edit below this line
