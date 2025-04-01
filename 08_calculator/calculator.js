a009538c7111234ad9187665a2123777c3b1db7b
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-04-01 22:19:24 +0200

    Calculator clean code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -4,21 +4,11 @@ const add = function(a, b) {
 
 const subtract = function(a, b) {
 	return a - b;
-};
+}
 
 const sum = function(array) {
-  if (array.length === 0){
-    return 0;
-  }   
-  else{
-      for(let i = 0; i < array.length; i++){
-      array[i] = parseFloat(array[i]);
-      return array.reduce((accumulator, item) =>  accumulator + item, 0);  
-    }
-  }
-
-  
-};
+  return array.reduce((accumulator, item) =>  accumulator + item, 0)
+}
 
 const multiply = function() {
 
