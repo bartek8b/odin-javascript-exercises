c4ef31f6c4988a9e171fd3332a50e1e40f15adca
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-04-02 22:33:08 +0200

    Rewrite palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const allowedChars = '1234567890qwertyuiopasdfghjklzxcvbnm';
+
+    const celanedString = word
+        .toLowerCase()
+        .split('')
+        .filter((item) => allowedChars.includes(item))
+        .join('');
+  
+    const reversedString = celanedString
+        .split('')
+        .reverse()
+        .join('');
+  
+    return (celanedString === reversedString);
 };
 
 // Do not edit below this line
