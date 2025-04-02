e82d97d2fdea6bdee559e1005d14ee86b5efda66
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-04-02 21:30:38 +0200

    Build fibonacci number algorythm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const array = [1, 1];    
 
+    num = Number(num);
+
+    if(!(num <= 2)){
+        for(let i = 0; i < num; i++){
+            array.push(array[i] + array [i + 1]);            
+        }
+    }
+    else{
+        if(num === 0){
+            return 0;
+        }
+        else if(num < 0){
+            return "OOPS";
+        }
+    }
+    
+    return array[num - 1];
 };
 
 // Do not edit below this line
