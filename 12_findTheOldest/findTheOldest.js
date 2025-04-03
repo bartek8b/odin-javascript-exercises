2bf3595a30523c30d1f265d2dac354bccb7e9adb
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-04-03 22:45:39 +0200

    Pass with function age()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,18 @@
 const findTheOldest = function(people) {
 
+    let age = function(person){
+        if (person.yearOfDeath === undefined){
+            return new Date().getFullYear() - person.yearOfBirth;
+        }
+        else{
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+
     const sorted = people
-                    .sort((prev, next) => (next.yearOfDeath - next.yearOfBirth) - (prev.yearOfDeath - prev.yearOfBirth));
+                    .sort((prev, next) => age(next) - age(prev));
     
     return sorted[0];
-
 };
 
 // Do not edit below this line
